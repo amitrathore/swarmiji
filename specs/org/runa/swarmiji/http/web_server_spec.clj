3204867419f5ccf0c34c6189104775af90bfb787
Amit Rathore <amitrathore@gmail.com>
2009-03-28 15:38:10 -0700

    first cut of applying web-params directly to handler-methods

---
+++ specs/org/runa/swarmiji/http/web_server_spec.clj
@@ -0,0 +1,5 @@
+(ns 'swarmiji-spec)
+
+(use 'clojure.contrib.test-is)
+(require '(org.runa.swarmiji.http [web-server as web-server]))
+
--- src/org/runa/swarmiji/http/web_server.clj
+++ src/org/runa/swarmiji/http/web_server.clj
@@ -1,42 +1,61 @@
 (ns org.runa.swarmiji.http.web-server)
 
 (import '(com.sun.grizzly.http SelectorThread))
-(import '(com.sun.grizzly.tcp Adapter OutputBuffer Request Response))
+(import '(com.sun.grizzly.tcp.http11 GrizzlyAdapter))
 (import '(com.sun.grizzly.util.buf ByteChunk))
 (import '(java.net HttpURLConnection))
 
 (defn is-get? [request]
-  (= (.toUpperCase (str (.method request))) "GET"))
+  (= (.toUpperCase (str (.getMethod request))) "GET"))
 
-(defn response-as-chunk [grizzly-response response-text]
-  (let [response-bytes (.getBytes response-text)
-	response-length (count response-bytes)
-	out-chunk (ByteChunk.)]
-    (.setStatus grizzly-response HttpURLConnection/HTTP_OK)
-    (.setContentLength grizzly-response response-length)
-    (.setContentType grizzly-response "text/plain")
-    (.append out-chunk response-bytes 0 response-length)
-    out-chunk))
+(defn requested-route-from [request]
+  (str (.getRequestURI request)))
 
-(defn service-http-request [custom-request-handler request response]
-  (do
-    (println "Recieved request from" (str (.requestURI request)))
-    (if (is-get? request)
-      (let [out-buffer (.getOutputBuffer response)
-	    response-text (custom-request-handler request)
-	    response-chunk (response-as-chunk response response-text)]
-	(.doWrite out-buffer response-chunk response)
-	(.finish response)))))
-	
-(defn after-service [request response]
-  (do
-    (.recycle request)
-    (.recycle response)))
+;(defn response-as-chunk [grizzly-response response-text]
+;  (let [response-bytes (.getBytes response-text)
+;	response-length (count response-bytes)
+;	out-chunk (ByteChunk.)]
+;    (.setStatus grizzly-response HttpURLConnection/HTTP_OK)
+;    (.setContentLength grizzly-response response-length)
+;    (.setContentType grizzly-response "text/plain")
+;    (.append out-chunk response-bytes 0 response-length)
+;    out-chunk))
 
-(defn http-request-handler [custom-request-handler]
-  (proxy [Adapter] []
+;(defn service-http-request [handler-functions-as-route-map request response]
+;  (if (is-get? request)
+;    (let [request-route (requested-route-from request)
+;	  route-handler (handler-functions-as-route-map request-route)
+;	  _ (println "Recieved request from" request-route)
+;	  _ (println "Request attributes:" (.getAttributes request))
+;	  out-buffer (.getOutputBuffer response)
+;	  response-text (route-handler request)
+;	  response-chunk (response-as-chunk response response-text)]
+;      (.doWrite out-buffer response-chunk response)
+;      (.finish response))))
+
+;(defn after-service [request response]
+;  (do
+;    (.recycle request)
+;    (.recycle response)))
+
+(defn params-for-dispatch [parameter-map]
+  (map first (.values parameter-map)))
+
+(defn service-http-request [handler-functions-as-route-map request response]
+  (if (is-get? request)
+    (let [request-route (requested-route-from request)
+	  route-handler (handler-functions-as-route-map request-route)
+	  params (params-for-dispatch (.getParameterMap request))
+	  _ (println "Recieved request from" request-route)
+	  _ (println "Request attributes:" params)
+	  response-text (apply route-handler params)]
+      (.println (.getWriter response) response-text))))
+
+(defn grizzly-adapter-for [handler-functions-as-route-map]
+  (proxy [GrizzlyAdapter] []
     (service [req res]
-      (service-http-request custom-request-handler req res))
-    (afterService [req res]
-      (after-service req res))))
+      (service-http-request handler-functions-as-route-map req res))))
+
+;    (afterService [req res]
+;      (after-service req res))))
 	     
