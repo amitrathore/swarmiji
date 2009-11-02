668d0ff7bf5c2608618a9c0fb63ec0690dd89f81
Kyle Oba <koba@mudphone.com>
2009-11-02 15:48:49 -0800

    add config file with new format required for summarizer ns

---
+++ config/config.koba.clj
@@ -0,0 +1,95 @@
+(defn current-swarmiji-ns []
+  (or swarmiji-ns
+      (throw (Exception. "SWARMIJI-NS is not set"))))
+
+(defn queue-name-for [stem]
+  (str stem (current-swarmiji-ns) "_" *swarmiji-env* "_"))
+
+(def operation-configs {
+    "test" {
+      :swarmiji-username "koba_on_rohanda"
+      :host "arryn.local"
+      :port 61613
+      :q-username "guest"
+      :q-password "guest"
+      :sevak-request-queue-prefix (queue-name-for "RUNA_SWARMIJI_TRANSPORT_")
+      :sevak-diagnostics-queue-prefix (queue-name-for "RUNA_SWARMIJI_DIAGNOSTICS_")    
+      :distributed-mode false
+      :diagnostics-mode true
+      :logsdir (str swarmiji-home "/logs")
+      :log-to-console true
+    }    
+    "development" {
+      :swarmiji-username "koba_on_rohanda"
+      :host "arryn.local"
+      :port 61613
+      :q-username "guest"
+      :q-password "guest"
+      :sevak-request-queue-prefix (queue-name-for "RUNA_SWARMIJI_TRANSPORT_")
+      :sevak-diagnostics-queue-prefix (queue-name-for "RUNA_SWARMIJI_DIAGNOSTICS_")    
+      :distributed-mode true
+      :diagnostics-mode true
+      :logsdir (str swarmiji-home "/logs")
+      :log-to-console true
+    }
+    "staging" {
+      :swarmiji-username "furtive"
+      :host "staging.cinchcorp.com"
+      :port 61613
+      :q-username "guest"
+      :q-password "guest"
+      :sevak-request-queue-prefix (queue-name-for "RUNA_SWARMIJI_TRANSPORT_")
+      :sevak-diagnostics-queue-prefix (queue-name-for "RUNA_SWARMIJI_DIAGNOSTICS_")    
+      :distributed-mode true
+      :diagnostics-mode true
+      :logsdir (str swarmiji-home "/mnt/pkgs/swarmiji/logs")
+      :log-to-console true
+    }
+    "production" {
+      :swarmiji-username "furtive"
+      :host "mq.production.runa.com"
+      :port 61613
+      :q-username "guest"
+      :q-password "guest"
+      :sevak-request-queue-prefix (queue-name-for "RUNA_SWARMIJI_TRANSPORT_")
+      :sevak-diagnostics-queue-prefix (queue-name-for "RUNA_SWARMIJI_DIAGNOSTICS_")    
+      :distributed-mode true
+      :diagnostics-mode true
+      :logsdir "/mnt/log/swarmiji/"
+      :log-to-console false
+    }
+  }
+)
+
+(def swarmiji-mysql-configs {
+      "development" {
+	     :classname "com.mysql.jdbc.Driver" 
+	     :subprotocol "mysql" 
+	     :user "root" 
+	     :password "password" 
+	     :subname (str "//localhost/swarmiji_development") 
+       }
+      "test" {
+	     :classname "com.mysql.jdbc.Driver" 
+	     :subprotocol "mysql" 
+       :user "root" 
+	     :password "password" 
+	     :subname (str "//localhost/swarmiji_development") 
+      }
+      "staging" {
+	     :classname "com.mysql.jdbc.Driver" 
+	     :subprotocol "mysql" 
+       :user "cinch" 
+	     :password "ind6678" 
+	     :subname (str "//staging.cinchcorp.com/swarmiji_staging") 
+      }
+      "production" {
+	     :classname "com.mysql.jdbc.Driver" 
+	     :subprotocol "mysql" 
+       :user "cinch" 
+	     :password "secret" 
+	     :subname (str "//db-primary.production.runa.com/swarmiji_production") 
+      }
+   }
+)
+
