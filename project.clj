8a6d34633329e2ded5ea480db97afb00ac5c6c86
George Jahad <george@runa.com>
2010-07-20 14:22:38 -0700

    updated for new medusa/clj-utils

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject swarmiji "0.3.0"
+(defproject swarmiji "0.3.1"
   :description "A distributed computing framework to help write and run Clojure code in parallel, across cores and processors"
   :url "http://github.com/amitrathore/swarmiji"
   :dependencies [[org.clojure/clojure "1.1.0"]
@@ -8,8 +8,8 @@
                  [mysql/mysql-connector-java "5.1.6"]
                  [rabbitmq-client "1.7.0"]
                  [org.clojars.sethtrain/postal "0.2.0"]
-                 [clj-utils "0.1.0"]
-                 [org.rathore.amit/medusa "0.1.0-SNAPSHOT"]
+                 [org.clojars.runa/clj-utils "0.1.2"]
+                 [org.clojars.runa/medusa "0.1.2"]
                  [org.clojars.macourtney/clj-record "1.0.1"]
                  [org.clojars.amit/swarmiji-java "0.2.0"]]
   :dev-dependencies [[swank-clojure "1.2.1"]])
