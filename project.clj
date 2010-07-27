c4a394960c71a5063a9548ce7dbce073d6a844c0
Amit Rathore <amitrathore@gmail.com>
2010-07-26 18:36:58 -0700

    updating clj-utils and medusa to 0.1.3

--- project.clj
+++ project.clj
@@ -8,8 +8,8 @@
                  [mysql/mysql-connector-java "5.1.6"]
                  [rabbitmq-client "1.7.0"]
                  [org.clojars.sethtrain/postal "0.2.0"]
-                 [org.clojars.runa/clj-utils "0.1.2"]
-                 [org.clojars.runa/medusa "0.1.2"]
+                 [org.clojars.runa/clj-utils "0.1.3"]
+                 [org.clojars.runa/medusa "0.1.3"]
                  [org.clojars.macourtney/clj-record "1.0.1"]
                  [org.clojars.amit/swarmiji-java "0.2.0"]]
   :dev-dependencies [[swank-clojure "1.2.1"]])
