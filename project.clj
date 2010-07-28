c0f69f44d7ee691a89b6a79bcf0dbc651495e42f
Amit Rathore <amitrathore@gmail.com>
2010-07-27 17:44:46 -0700

    bumped version to 0.3.3 and of dep medusa to 0.1.5

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject swarmiji "0.3.2"
+(defproject swarmiji "0.3.3"
   :description "A distributed computing framework to help write and run Clojure code in parallel, across cores and processors"
   :url "http://github.com/amitrathore/swarmiji"
   :dependencies [[org.clojure/clojure "1.1.0"]
@@ -9,7 +9,7 @@
                  [rabbitmq-client "1.7.0"]
                  [org.clojars.sethtrain/postal "0.2.0"]
                  [org.clojars.runa/clj-utils "0.1.3"]
-                 [org.clojars.runa/medusa "0.1.4"]
+                 [org.clojars.runa/medusa "0.1.5"]
                  [org.clojars.macourtney/clj-record "1.0.1"]
                  [org.clojars.amit/swarmiji-java "0.2.0"]]
   :dev-dependencies [[swank-clojure "1.2.1"]])
