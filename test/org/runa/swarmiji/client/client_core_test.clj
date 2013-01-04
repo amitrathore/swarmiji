4c245d9a0b9d628996b53637364f3c7f471da2e4
Alex Baranosky <alexander.baranosky@gmail.com>
2013-01-04 12:34:42 -0800

    moved /specs tests into /test

--- test/org/runa/swarmiji/client/client_core_test.clj
+++ test/org/runa/swarmiji/client/client_core_test.clj
@@ -1,6 +1,6 @@
-(ns org.runa.swarmiji.client.client-core-spec
-  (:use [clojure.test :only [deftest is run-tests]])
-  (:use org.runa.swarmiji.mpi.transport
+(ns org.runa.swarmiji.client.client-core-test
+  (:use [clojure.test :only [deftest is run-tests]]
+        org.runa.swarmiji.mpi.transport
         org.runa.swarmiji.sevak.sevak-core
         org.runa.swarmiji.client.client-core
         org.runa.swarmiji.sevak.bindings))
@@ -16,6 +16,9 @@
 (defsevak inc-atom [n]
   (swap! test-atom + n))
 
+;; TODO - Alex/Francis - Jan 4, 2012 - This test was resurrected from the specs
+;; folder.  It looks like it needs rabbit up to run.  So as currently setup
+;; this will always fail.
 (deftest test-retry-sevaks
   (reset! test-atom 0)
   (let [s1 (inc-atom 3)
@@ -23,4 +26,3 @@
     (is (= @test-atom 8))
     (retry-sevaks 10 inc-atom [s1 s2 (incomplete-sevak s1) (incomplete-sevak s2)])
     (is (= @test-atom 16))))
-
