82c37d48b3d3b2a7b029c3a221af898e7e7bd6fd
Alex Baranosky <alexander.baranosky@gmail.com>
2013-01-04 12:36:03 -0800

    contrib no longer appears in classpath; up version

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject swarmiji "0.4.3"
+(defproject swarmiji "0.4.4"
   :description "A distributed computing framework to help write and run Clojure code in parallel, across cores and processors"
   :url "http://github.com/runa-dev/swarmiji"
   :dependencies [[org.clojure/clojure            "1.4.0"]
                  [mysql/mysql-connector-java     "5.1.6"]
                  [com.rabbitmq/amqp-client       "2.5.0"]
-                 [clj-utils                      "1.2.2"]
-                 [medusa                         "0.1.9"]
+                 [clj-utils                      "1.2.3"]
+                 [medusa                         "0.1.10"]
                  [clj-record                     "1.1.4"]
                  [org.clojars.amit/swarmiji-java "0.2.0"]]
   :dev-dependencies [[slamhound                "1.3.1"]
